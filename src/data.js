d6bb1ff699b6fb7a71fd84a4ff392a3c67d938f7
ashusri33 <ashutoshsrivastava1833@gmail.com>
2023-09-02 20:36:06 +0530

    First commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+const reviews=[
+    {
+        id:1,
+        name:"Pranay Gupta",
+        job:"Software Engineer",
+        image:"https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text:"The program's comprehensive curriculum and hands-on projects enriched my skills. Collaborative learning nurtured teamwork, while career support ensured a seamless transition. Grateful for the lasting impact on my journey as a proficient software engineer"
+    },
+    {
+        id: 2,
+        name:"Abir Pal",
+        job:"Graphic Designer",
+        image:"https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text:"As a graphic designer, my creativity and technical skills. Diverse projects and expert guidance enriched my design perspective. Grateful for the foundation to excel in a dynamic field."
+    },
+    {
+        id:3,
+        name:"Soumya Banerjee",
+        job:"Marketing Manager",
+        image:"https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text:"My role as a marketing manager has been great. Comprehensive insights into consumer behavior and strategic planning have elevated my decision-making. Thankful for the impactful learning journey"
+    },
+    {
+        id:4,
+        name:"Saiket Mukherjee",
+        job:"Content Strategist",
+        image:"https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text:"Transformative is how I describe my experience as a content strategist. In-depth strategies, creative storytelling, and digital insights have refined my approach. Grateful for the skills shaping my success."
+    },
+    {
+        id:5,
+        name:"Aritra Biswas",
+        job:"Data Analyst",
+        image:"https://aeccc.targettechnology.in/static/media/AritraBiswas.c0cf92b76053d1f0b939.jpg",
+        text:"Institute played a pivotal role in shaping my journey as a data analyst. From statistical techniques to data visualization, the program's comprehensive curriculum equipped me with essential skills. Grateful for the solid foundation enhancing my analytical career."
+    },
+]
+export default reviews; 
